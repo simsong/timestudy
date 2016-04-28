bf167ddf19fcf61e068aad3b8721ae04f795cfff
Simson Garfinkel <simsong@acm.org>
2016-04-28 15:31:30 -0400

    handles time study

--- webtime.py
+++ webtime.py
@@ -5,58 +5,102 @@
 import csv
 import time
 
+MIN_TIME = 3
+
+def ip2long(ip):
+    import socket,struct
+    """
+    Convert an IP string to long
+    """
+    packedIP = socket.inet_aton(ip)
+    return struct.unpack("!L", packedIP)[0]
+
+
+class WebTime():
+    def __init__(self,qhost=None,qtime=None,delta=None,qipaddr=None,rdate=None,code=None):
+        self.qhost = qhost
+        self.qtime = qtime
+        self.qipaddr = qipaddr
+        self.rdate  = rdate
+        self.delta = delta
+        self.code  = code
+    def ptime(self):
+        if self.delta < 0:
+            sign = "-"
+            dt = -self.delta
+        else:
+            sign = " "
+            dt = self.delta
+        sec  = int(dt % 60)
+        min  = int((dt/60) % 60)
+        hour = int(dt / 3600)
+        return "{:1}{:02}:{:02}:{:02}".format(sign,hour,min,sec)
+
+def webtime_ip(host,ipaddr):
+    import http,socket,email,sys
+    """Find the webtime of a particular host and IP address"""
+    RemoteDisconnected = http.client.BadStatusLine
+    if sys.version>'3.5':
+        RemoteDisconnected = http.client.RemoteDisconnected
+    connection = http.client.HTTPConnection(ipaddr,timeout=5)
+    try:
+        url = "http://{}/".format(host)
+        connection.request("HEAD",url)
+        r = connection.getresponse()
+    except socket.gaierror:
+        return
+    except socket.timeout:
+        return
+    except http.client.BadStatusLine:
+        return
+    except ConnectionResetError:
+        return
+    except OSError:
+        return
+    except RemoteDisconnected:
+        return
+    t = time.time()
+    val = r.getheader("Date")
+    if val:
+        date = email.utils.parsedate_to_datetime(val)
+        return WebTime(qhost=host,qtime=t,qipaddr=ipaddr,
+                      rdate=val,delta=date.timestamp()-t,code=r.code)
+    else:
+        print("No date for {} {}".format(host,ipaddr))
+        print(r.getheaders())
+
+
 def webtime(host):
     import time
+    import http
     from http import client
     import email
     import datetime
     import socket
-
-    t0 = time.time()
+    import dns.resolver
+    import sys
 
     prefixes = ["","","","www.","www.","www.","www1.","www2.","www3."]
     prefixes = [""]
 
     for prefix in prefixes:
         qhost = prefix+host
-        print("{}\t\t\r".format(qhost),end="")
-        connection = client.HTTPConnection(qhost,timeout=5)
         try:
-            connection.request("HEAD","/")
-            response = connection.getresponse()
-        except client.RemoteDisconnected:
+            a = dns.resolver.query(qhost,"A")
+        except dns.resolver.NoAnswer:
             continue
-        except socket.gaierror:
-            continue
-        except socket.timeout:
-            continue
-        except NameError:
-            continue
-        except ConnectionResetError:
-            continue
-        except client.BadStatusLine:
-            continue
-        except OSError:
-            continue
-        t1 = time.time()
-        for (key,val) in filter(lambda r:r[0]=='Date',response.getheaders()):
-            if val:
-                date = email.utils.parsedate_to_datetime(val)
-                yield (qhost,val,date.timestamp(),date.timestamp()-time.time())
+        for r in a:
+            ipaddr = r.to_text()
+            for i in range(3):
+                w = webtime_ip(qhost, ipaddr)
+                if w.delta < MIN_TIME:
+                    break
+                yield w
 
 def queryhost(vax):
     (rank,host) = vax
-    for (qhost,val,t,dt) in webtime(host):
-        sign = " "
-        if dt<0:
-            dt = -dt;
-            sign = "-"
-        if dt<5:
-            continue
-        sec  = int(dt % 60)
-        min  = int((dt/60) % 60)
-        hour = int(dt / 3600)
-        print("{:4} {:30} {:30} {}{:02}:{:02}:{:02}".format(rank,qhost,val,sign,hour,min,sec))    
+    for wt in webtime(host):
+        print("{:4} {:30} {:20} {:30} {}".format(rank,wt.qhost,wt.qipaddr,wt.ptime(),wt.rdate))
 
 
 
@@ -74,6 +118,9 @@ if __name__=="__main__":
         urls.append(line)
         if len(urls)>count:
             break
+    for u in urls: queryhost(u)
+    exit(0)
+
     pool = Pool(15)
     results = pool.map(queryhost,urls)
     end = time.time()
