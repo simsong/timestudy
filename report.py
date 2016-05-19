f0669551bed5b6d83ba8f933be4ad2e035409aee
Simson Garfinkel <simsong@acm.org>
2016-05-19 00:24:55 +0000

    updated

---
+++ report.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+#https://support.alexa.com/hc/en-us/articles/200461990-Can-I-get-a-list-of-top-sites-from-an-API-
+#http://s3.amazonaws.com/alexa-static/top-1m.csv.zip
+
+import os
+import csv
+import time,datetime,pytz
+import pymysql, pymysql.err
+import MySQLdb, _mysql_exceptions
+import subprocess
+import sys
+import math
+
+CONFIG_INI = "config.ini"
+
+def gen_report(conn,domains,smin,smax,desc):
+    domains_str = ",".join(["'{}'".format(s) for s in domains])
+    c = conn.cursor()
+    c.execute("select host,ipaddr,qdatetime,delta from times where host in ("+domains_str+") order by host,ipaddr;")
+    for row in c.fetchall():
+        print(row)
+    
+                                                                                      
+
+if __name__=="__main__":
+    import argparse
+    from bs4 import BeautifulSoup, SoupStrainer
+    import webtime
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--usg',action='store_true')
+    parser.add_argument("--debug",action="store_true",help="write results to STDOUT")
+    parser.add_argument("--verbose",action="store_true",help="output to STDOUT")
+    parser.add_argument("--mysql",action="store_true",help="output to MySQL DB")
+    parser.add_argument("--mongo",action="store_true",help="output to MongoDB")
+    parser.add_argument("--config",help="config file",default=CONFIG_INI)
+    parser.add_argument("--host",help="Specify a host")
+
+    args = parser.parse_args()
+    config = webtime.get_config(args)
+
+    # Make sure mySQL works
+    if args.mysql:
+        w = webtime.WebLogger(args.debug)
+        w.mysql_config = config["mysql"]
+        conn = w.mysql_connect(cache=False)       # test it out
+        if args.debug: print("MySQL Connected")
+
+    domains = webtime.usg_domains()
+    #
+    # 
+    # Time in ranges
+    gen_report(conn,domains,1,59,"< minute")
--- webtime.py
+++ webtime.py
@@ -9,6 +9,7 @@ import pymysql, pymysql.err
 import MySQLdb, _mysql_exceptions
 import subprocess
 import sys
+import math
 
 MIN_TIME = 1.0                # Resolution of remote websites
 CONFIG_INI = "config.ini"
@@ -17,6 +18,19 @@ mysql_schema = """
 """
 
 prefixes = ["","","","www.","www.","www.","www1.","www2.","www3."]
+
+def get_config(args):
+    import configparser
+    config = configparser.ConfigParser()
+    config["mysql"] = {"host":"localhost",
+                       "user":"user",
+                       "passwd":"",
+                       "port":3306,
+                       "db":"timedb",
+                       "mysqldump":"mysqldump" }
+    config.read(args.config)
+    return config
+
 
 def ip2long(ip):
     import socket,struct
@@ -29,16 +43,28 @@ def ip2long(ip):
 
 class WebTime():
     """Webtime class. qdatetime is a datetime object when the query was made, rdatetime is the datetime returned by the remote system."""
-    def __init__(self,qhost=None,qipaddr=None,qdatetime=None,qduration=None,rdatetime=None,rcode=None):
-        self.qhost = qhost
-        self.qipaddr= qipaddr
-        self.qdatetime  = qdatetime
-        self.qduration = qduration
-        self.rdatetime  = rdatetime
-        self.rcode  = rcode
+    def __init__(self,qhost=None,qipaddr=None,qdatetime=None,qduration=None,rdatetime=None,rcode=None,dateline=None):
+        def fixtime(dt):
+            try:
+                return dt.astimezone(pytz.utc)
+            except ValueError:
+                return dt.replace(tzinfo=pytz.utc)
+        self.qhost      = qhost
+        self.qipaddr    = qipaddr
+        self.qdatetime  = fixtime(qdatetime)
+        self.qduration  = qduration
+        self.rdatetime  = fixtime(rdatetime)
+        self.rcode      = rcode
+        self.dateline   = dateline
+
+        # Make sure that datetimes are aware
 
     def delta(self):
-        return self.qdatetime - self.rdatetime
+        try:
+            return self.qdatetime - self.rdatetime
+        except TypeError as e:
+            print("Bad wt: {}".format(self))
+            raise e
 
     def delta_seconds(self):
         return self.delta().total_seconds()
@@ -65,19 +91,38 @@ class WebTime():
     def qtime(self):
         return self.qdatetime.time().isoformat()
     def __repr__(self):
-        return "<WebTime {} {} {}>".format(self.qhost,self.qdatetime,self.delta_seconds())
+        return "<WebTime {} {} {} {}>".format(self.qhost,self.qipaddr,self.qdatetime,self.rdatetime)
+
+def webtime_off(wt):
+    return math.fabs(wt.delta_seconds()) > args.mintime if wt else None
+
+def usg_domains():
+    from bs4 import BeautifulSoup, SoupStrainer
+    domains = []
+    import urllib, urllib.request
+    page = urllib.request.urlopen("http://usgv6-deploymon.antd.nist.gov/cgi-bin/generate-gov.v4").read()
+    for link in BeautifulSoup(page, "lxml", parse_only=SoupStrainer('a')):
+        try:
+            import urllib
+            o = urllib.parse.urlparse(link.attrs['href'])
+            if o.netloc: domains.append(o.netloc)
+        except AttributeError:
+            pass
+    return domains
+    
 
 class WebLogger:
-    def __init__(self):
+    def __init__(self,debug=False):
         self.mysql_config = None
         self.connected = None
+        self.debug     = debug
 
     def mysql_connect(self,cache=False):
         if self.connected:
             return self.connected
         mc = self.mysql_config
         try:
-            if args.debug: print("Connected in PID {}".format(os.getpid()))
+            if self.debug: print("Connected in PID {}".format(os.getpid()))
             conn = pymysql.connect(host=mc["host"],port=int(mc["port"]),user=mc["user"],
                                    passwd=mc['passwd'],db=mc['db'])
             #conn = MySQLdb.connect(host=mc["host"],port=int(mc["port"]),user=mc["user"],
@@ -137,16 +182,16 @@ class WebLogger:
                 r = connection.getresponse()
                 t1 = time.time()
             except socket.gaierror:
-                if args.debug: print("ERROR socket.gaierror {} {}".format(domain,ipaddr))
+                if self.debug: print("ERROR socket.gaierror {} {}".format(domain,ipaddr))
                 continue
             except socket.timeout:
-                if args.debug: print("ERROR socket.timeout {} {}".format(domain,ipaddr))
+                if self.debug: print("ERROR socket.timeout {} {}".format(domain,ipaddr))
                 continue
             except http.client.BadStatusLine:
-                if args.debug: print("ERROR http.client.BadStatusLine {} {}".format(domain,ipaddr))
+                if self.debug: print("ERROR http.client.BadStatusLine {} {}".format(domain,ipaddr))
                 continue
             except ConnectionResetError:
-                if args.debug: print("ERROR ConnectionResetError {} {}".format(domain,ipaddr))
+                if self.debug: print("ERROR ConnectionResetError {} {}".format(domain,ipaddr))
                 continue
             except OSError:
                 continue
@@ -160,14 +205,18 @@ class WebLogger:
             qduration = t1-t0
             qdatetime = datetime.datetime.fromtimestamp(t0+qduration/2,pytz.utc)
             return WebTime(qhost=domain,qipaddr=ipaddr,qdatetime=qdatetime,qduration=qduration,
-                           rdatetime=date,rcode=r.code)
+                           rdatetime=date,rcode=r.code,dateline=date)
         # Too many retries
-        if args.debug: print("ERROR too many retries")
+        if self.debug: print("ERROR too many retries")
         return None
         
 
     def webtime(self,qhost,c):
-        """Given the domain, get the IP addresses and query each one. Return the list of IP addresses that had bad times"""
+        """
+        Given the domain, get the IP addresses and query each one. 
+        Updates the dated table.
+        Return the web time for each IP address.
+        """
         import time
         import http
         from http import client
@@ -191,43 +240,47 @@ class WebLogger:
                                (qhost,qdate))
             host_id = c.fetchone()
             if host_id:
+                # Update the query count for the hostname
                 self.mysql_execute(c,"update dated set qlast=%s,qcount=qcount+1 where id=%s",(qtime,host_id))
 
         try:
-            if args.debug: print("DEBUG qhost={}".format(qhost))
+            if self.debug: print("DEBUG qhost={}".format(qhost))
             a = socket.gethostbyname_ex(qhost)
             ipaddrs = a[2]
-            if args.debug: print("DEBUG   qhost={} ipaddrs={}".format(qhost,ipaddrs))
+            if self.debug: print("DEBUG   qhost={} ipaddrs={}".format(qhost,ipaddrs))
         except socket.gaierror:
             if host_id: self.mysql_execute(c,"update dated set qlast=%s,ecount=ecount+1 where id=%s",(qtime,host_id))
-            if args.debug: print("ERROR socket.aierror {} ".format(qhost))
+            if self.debug: print("ERROR socket.aierror {} ".format(qhost))
             return
         except socket.herror:
             if host_id: self.mysql_execute(c,"update dated set qlast=%s,ecount=ecount+1 where id=%s",(qtime,host_id))
-            if args.debug: print("ERROR socket.herror {}".format(qhost))
+            if self.debug: print("ERROR socket.herror {}".format(qhost))
             return
         # Try each IP address
         for ipaddr in set(ipaddrs): # do each one once
+            # Query the IP address
             wt = self.webtime_ip(qhost, ipaddr)
-            if args.debug: 
+            if self.debug: 
                 print("DEBUG   qhost={} ipaddr={:15} wt={}".format(qhost,ipaddr,wt))
+            if c and wt:
+                # Note that we are going to query this IP address (again)
+                self.mysql_execute(c,"insert ignore into dated (host,ipaddr,qdate,qfirst) values (%s,%s,%s,%s)",
+                                   (wt.qhost,wt.qipaddr,wt.qdate(),wt.qtime()))
+                self.mysql_execute(c,"select id from dated where host=%s and ipaddr=%s and qdate=%s",
+                                   (wt.qhost,wt.qipaddr,wt.qdate()))
+                ip_id = c.fetchone()[0]
+                self.mysql_execute(c,"update dated set qlast=%s,qcount=qcount+1 where id=%s",(wt.qtime(),ip_id))
+            if c and webtime_off(wt):
+                # We got a response and it's the wrong time
+                self.mysql_execute(c,"update dated set wtcount=wtcount+1 where id=%s",(ip_id))
             if wt:
-                if c:
-                    t0 = time.time()
-                    self.mysql_execute(c,"insert ignore into dated (host,ipaddr,qdate,qfirst) values (%s,%s,%s,%s)",
-                                       (wt.qhost,wt.qipaddr,wt.qdate(),wt.qtime()))
-                    self.mysql_execute(c,"select id from dated where host=%s and ipaddr=%s and qdate=%s",
-                                       (wt.qhost,wt.qipaddr,wt.qdate()))
-                    ip_id = c.fetchone()
-                    if ip_id:
-                        self.mysql_execute(c,"update dated set qlast=%s,qcount=qcount+1 where id=%s",(wt.qtime(),ip_id[0]))
-                        self.mysql_execute(c,"update dated set wtcount=wtcount+1 where id=%s",(ip_id))
+                # We got a wrong time
                 yield wt
 
 
 
     def queryhost(self,qhost):
-        """Query the host and store them in a database if they are interesting. Update the database to indicate when the host was surveyed"""
+        """Query the host and stores the bad time reads in the times database if they are off."""
         import os,math
 
         c = None
@@ -240,8 +293,8 @@ class WebLogger:
             c = conn.cursor()
 
         for wt in self.webtime(qhost,c):
-            # Note that we successfully queried this IP address
-            if math.fabs(wt.delta_seconds()) > args.mintime:
+            # Note if the webtime is off.
+            if webtime_off(wt):
                 if args.verbose: 
                     print("{:35} {:20} {:30} {}".format(wt.qhost,wt.qipaddr,wt.pdiff(),wt.rdatetime))
                 self.mysql_execute(c,"insert into times (host,ipaddr,qdatetime,qduration,rdatetime,delta) "+
@@ -254,7 +307,6 @@ class WebLogger:
 
 if __name__=="__main__":
     import argparse
-    from bs4 import BeautifulSoup, SoupStrainer
     import configparser
 
     parser = argparse.ArgumentParser()
@@ -273,18 +325,16 @@ if __name__=="__main__":
     parser.add_argument("--repeat",type=int,help="Times to repeat experiment")
     parser.add_argument("--norepeat",action="store_true",help="Used internally to implement repeating")
     parser.add_argument("--mysql_max",type=int,default=0,help="Number of MySQL connections before reconnecting")
+    parser.add_argument("--dumpschema",action="store_true")
 
     args = parser.parse_args()
+    config = get_config(args)
 
-    config = configparser.ConfigParser()
-    config["mysql"] = {"host":"localhost",
-                       "user":"user",
-                       "passwd":"",
-                       "port":3306,
-                       "db":"timedb"}
-    config.read(args.config)
+    if args.dumpschema:
+        call([config['mysqldump'],'-h',config['host'],'-u',config['user'],'-p',config['passwd'],'-d',config['db']])
+        exit(0)
 
-    w = WebLogger()
+    w = WebLogger(args.debug)
 
     # Make sure mySQL works
     if args.mysql:
@@ -298,7 +348,7 @@ if __name__=="__main__":
             print("**************************************")
             print("**************** {:4} ****************".format(r))
             print("**************************************")
-            print(time.localtime())
+            print(time.asctime())
             subprocess.call([sys.executable] + sys.argv + ["--norepeat"])
         exit(0)
 
@@ -312,15 +362,7 @@ if __name__=="__main__":
     # Get the list of URLs to check
     #
     if args.usg:
-        import urllib, urllib.request
-        page = urllib.request.urlopen("http://usgv6-deploymon.antd.nist.gov/cgi-bin/generate-gov.v4").read()
-        for link in BeautifulSoup(page, "lxml", parse_only=SoupStrainer('a')):
-            try:
-                import urllib
-                o = urllib.parse.urlparse(link.attrs['href'])
-                if o.netloc: domains.append(o.netloc)
-            except AttributeError:
-                pass
+        domains = usg_domains()
         print("Total USG: {}".format(len(domains)))
 
     if not domains:
