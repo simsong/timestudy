8f94f5868a8abf390791fb113468e90f40572c54
Simson Garfinkel <simsong@acm.org>
2017-10-13 01:30:04 +0000

    modified to use requests

--- hosts_usg.py
+++ hosts_usg.py
@@ -3,9 +3,8 @@
 # library for dealing with USG hosts
 
 
+import requests
 import glob
-import urllib
-import urllib.request
 import re
 from bs4 import BeautifulSoup
 
@@ -17,25 +16,16 @@ def url_to_hostname(s):
 
 def usg_from_nist():
     """Return a list of USG hosts from the NIST setting"""
-    page = urllib.request.urlopen("http://usgv6-deploymon.antd.nist.gov/cgi-bin/generate-gov.v4").read()
-    soup = BeautifulSoup(page,'lxml')
+    r = requests.get("http://usgv6-deploymon.antd.nist.gov/cgi-bin/generate-gov.v4")
+    soup = BeautifulSoup(r.text,'lxml')
 
     return [ url_to_hostname( link.get('href') )
              for link in soup.findAll('a', attrs={'href': re.compile("^https?://")})]
 
 def usg_from_analytics():
     URL = "https://analytics.usa.gov/data/live/sites.csv"
-    req = urllib.request(URL,headers={'Accept-encoding':'identity'})
-    data = req.urlopen(URL).read()
-    print("data:",data)
-    exit(0)
-
-    resource = urllib.request.urlopen(URL)
-    print("resource=",resource)
-    print("resource.headers.get_content_charset()=",resource.headers.get_content_charset())
-    print("resource.read()=",resource.read().decode('utf-8'))
-    contents  = resource.read().decode(resource.headers.get_content_charset())
-    return contents.splitlines()
+    r = requests.get(URL)
+    return r.text.splitlines()
 
 
 if __name__=="__main__":
