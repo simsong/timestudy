e70ebe264782a634273e34afc0f8171f0e61e569
Simson L. Garfinkel <simsong@acm.org>
2018-02-18 00:33:21 +0000

    fixed conflict

--- misc/CONFIGURE_CENTOS.sh
+++ misc/CONFIGURE_CENTOS.sh
@@ -7,20 +7,22 @@ sudo yum -y install git         # you probably already did this
 sudo yum -y update
 sudo yum -y makecache fast 
 sudo yum -y install deltarpm yum-cron
-sudo yum -y install emacs aspell aspell-en
+sudo yum -y install emacs mailx aspell aspell-en
 sudo yum -y install zlib zlib-static
-
 sudo yum -y install mariadb mariadb-server mariadb-devel mariadb-libs 
-sudo yum -y install httpd
-sudo yum -y install gcc
 sudo yum -y groupinstall 'Development Tools'
 sudo systemctl start mariadb
 sudo systemctl status mariadb
 sudo systemctl enable mariadb
+# Web server
+sudo yum -y install httpd
+
 
 ## Now figure out how to install Python3
 if grep Amazon /etc/system-release ; then
   sudo amazon-linux-extras install python3=lates
+  sudo usermod -a -G apache `whoami`
+  sudo chmod g+w /var/www/html
 else
   # Install Python3 from EPEL Repository
   # http://ask.xmodulo.com/install-python3-centos.html
@@ -40,8 +42,10 @@ sudo pip3 install matplotlib pytest bs4 lxml dnspython pytest requests
   # https://stackoverflow.com/questions/46215390/unable-to-find-protobuf-include-directory-when-i-use-pip-install-mysql-connec
 sudo pip3 install mysql-connector==2.1.4
 
-
 # Fix for selinux
 # https://serverfault.com/questions/322117/selinux-letting-apache-talk-to-mysql-on-centos
 sudo setsebool -P httpd_enable_cgi 1
 sudo setsebool -P httpd_can_network_connect 1
+
+# Set up cron
+crontab ../etc/crontab.txt
