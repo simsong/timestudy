775cbdd48f05d639ef9a0eed12c66c6ca6ba6772
EC2 Default User <ec2-user@ip-172-31-82-179.ec2.internal>
2018-02-18 00:32:43 +0000

    fixed conflict

--- daily.bash
+++ daily.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 #
 # Daily maintenance
-make backup && mv database.sql.gz /var/www/html && chcon -t httpd_sys_rw_content_t /var/www/html/database.sql.gz
+make backup && mv database.sql.gz /var/www/html && sudo chcon -R -t httpd_sys_rw_content_t /var/www/html
 
